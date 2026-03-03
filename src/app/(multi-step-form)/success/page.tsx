de06a59c9c353bb89c7ea7ebe5602e06968e8f1a
NextZeD21 <ghazaliyassine50@gmail.com>
2026-03-03 12:46:33 +0000

    6

--- src/app/(multi-step-form)/success/page.tsx
+++ src/app/(multi-step-form)/success/page.tsx
@@ -1,62 +1,225 @@
 import ButtonLink from "@/app/components/buttons/ButtonLink";
+import { SVGProps } from "react";
+
+export function IconCheck(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={60}
+      height={60}
+      viewBox="0 0 60 60"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M53.8631 12.7779C54.5661 13.4811 54.9611 14.4348 54.9611 15.4292C54.9611 16.4235 54.5661 17.3772 53.8631 18.0804L25.7556 46.1879C25.3842 46.5594 24.9432 46.8542 24.4578 47.0552C23.9724 47.2563 23.4522 47.3598 22.9269 47.3598C22.4015 47.3598 21.8813 47.2563 21.3959 47.0552C20.9106 46.8542 20.4696 46.5594 20.0981 46.1879L6.13311 32.2254C5.77495 31.8795 5.48927 31.4657 5.29273 31.0082C5.0962 30.5507 4.99275 30.0586 4.98842 29.5607C4.9841 29.0627 5.07898 28.5689 5.26753 28.1081C5.45608 27.6472 5.73453 27.2285 6.08663 26.8764C6.43873 26.5243 6.85742 26.2459 7.31828 26.0573C7.77915 25.8688 8.27294 25.7739 8.77087 25.7782C9.26879 25.7825 9.76086 25.886 10.2184 26.0825C10.6759 26.2791 11.0897 26.5647 11.4356 26.9229L22.9256 38.4129L48.5581 12.7779C48.9064 12.4294 49.3199 12.153 49.775 11.9644C50.2301 11.7758 50.718 11.6787 51.2106 11.6787C51.7033 11.6787 52.1911 11.7758 52.6462 11.9644C53.1014 12.153 53.5149 12.4294 53.8631 12.7779Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+export function IconDownload(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={28}
+      height={28}
+      viewBox="0 0 28 28"
+      fill="none"
+    >
+      <path
+        d="M21 23.625C21.4832 23.625 21.875 24.0168 21.875 24.5C21.875 24.9832 21.4832 25.375 21 25.375H7C6.51675 25.375 6.125 24.9832 6.125 24.5C6.125 24.0168 6.51675 23.625 7 23.625H21Z"
+        fill="#0F110C"
+      />
+      <path
+        d="M14 2.625C14.4832 2.625 14.875 3.01675 14.875 3.5V17.721L19.2147 13.3813C19.5564 13.0396 20.1103 13.0396 20.452 13.3813C20.7937 13.7231 20.7937 14.2769 20.452 14.6187L14.6187 20.452C14.2769 20.7937 13.7231 20.7937 13.3813 20.452L7.54801 14.6187C7.20631 14.2769 7.20631 13.7231 7.54801 13.3813C7.88972 13.0396 8.44361 13.0396 8.78532 13.3813L13.125 17.721V3.5C13.125 3.01675 13.5168 2.625 14 2.625Z"
+        fill="#0F110C"
+      />
+    </svg>
+  );
+}
+export function IconInfo(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M8 6.83333C8.27614 6.83333 8.5 7.05719 8.5 7.33333V10.6667C8.5 10.9428 8.27614 11.1667 8 11.1667C7.72386 11.1667 7.5 10.9428 7.5 10.6667V7.33333C7.5 7.05719 7.72386 6.83333 8 6.83333Z"
+        fill="#4C5242"
+      />
+      <path
+        d="M8.0332 4.83333C8.30935 4.83333 8.5332 5.05719 8.5332 5.33333V5.39974C8.5332 5.67539 8.31012 5.89903 8.03451 5.89974L7.9681 5.90039C7.83532 5.90065 7.70726 5.8477 7.61328 5.75391C7.5195 5.66019 7.46687 5.53297 7.4668 5.40039V5.33333C7.4668 5.05719 7.69065 4.83333 7.9668 4.83333H8.0332Z"
+        fill="#4C5242"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 1.5C11.5899 1.5 14.5 4.41015 14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8C1.5 4.41015 4.41015 1.5 8 1.5ZM8 2.5C4.96243 2.5 2.5 4.96243 2.5 8C2.5 11.0376 4.96243 13.5 8 13.5C11.0376 13.5 13.5 11.0376 13.5 8C13.5 4.96243 11.0376 2.5 8 2.5Z"
+        fill="#4C5242"
+      />
+    </svg>
+  );
+}
+export function IconRefresh(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M3.33203 9.37467C3.67721 9.37467 3.95703 9.6545 3.95703 9.99967C3.95703 11.602 4.5932 13.1391 5.72624 14.2721C6.85889 15.4048 8.39531 16.0401 9.99707 16.0405C11.6992 16.0339 13.333 15.3706 14.5568 14.1875L15.1566 13.5877H12.9618C12.6166 13.5877 12.3368 13.3079 12.3368 12.9627C12.3368 12.6175 12.6166 12.3377 12.9618 12.3377H16.6654C17.0105 12.3377 17.2904 12.6175 17.2904 12.9627V16.6663C17.2904 17.0115 17.0105 17.2913 16.6654 17.2913C16.3202 17.2913 16.0404 17.0115 16.0404 16.6663V14.4715L15.4259 15.0859C13.97 16.4934 12.0261 17.2837 10.0011 17.2913H9.9987C8.06483 17.2913 6.2099 16.5234 4.84245 15.1559C3.475 13.7885 2.70703 11.9335 2.70703 9.99967C2.70703 9.6545 2.98685 9.37467 3.33203 9.37467Z"
+        fill="#0F110C"
+      />
+      <path
+        d="M9.9987 2.70801C11.9326 2.70801 13.7875 3.47597 15.1549 4.84342C16.5224 6.21088 17.2904 8.0658 17.2904 9.99967C17.2904 10.3449 17.0105 10.6247 16.6654 10.6247C16.3202 10.6247 16.0404 10.3449 16.0404 9.99967C16.0404 8.39733 15.4042 6.86025 14.2712 5.72721C13.1383 4.59437 11.6016 3.95822 9.99951 3.95801C8.29766 3.96481 6.66423 4.62899 5.44059 5.81185L4.84082 6.41162H7.03564C7.38079 6.41162 7.6606 6.69148 7.66064 7.03662C7.66064 7.3818 7.38082 7.66162 7.03564 7.66162H3.33203C2.98685 7.66162 2.70703 7.3818 2.70703 7.03662V3.33301C2.70703 2.98783 2.98685 2.70801 3.33203 2.70801C3.67721 2.70801 3.95703 2.98783 3.95703 3.33301V5.52783L4.57145 4.91341C6.02737 3.50594 7.97126 2.71565 9.99626 2.70801H9.9987Z"
+        fill="#0F110C"
+      />
+    </svg>
+  );
+}
 
 export default function SuccessPage() {
   return (
-    <section className="py-[200px] max-tablet:py-[100px] px-4 w-full flex-center flex-col gap-6 ">
+    <section className="relative pt-[200px] pb-11 px-4 w-full flex-center flex-col gap-6 ">
       <div
-        className="absolute inset-0 w-full h-[508px] bg-green-100 -z-[1]
+        className="absolute inset-0 w-full h-[200px] bg-green-100 -z-[1]
         bg-[url('/success-overlay.png'),linear-gradient(180deg,var(--color-Primary-300)_0%,var(--color-white)_100%)] 
-        bg-top bg-no-repeat max-tablet:bg-cover"
-      ></div>
-      <div className="flex p-10 rounded-full bg-Primary-300">
-        <div className="flex p-6 rounded-full bg-Primary-500/50">
-          <div className="flex p-5 rounded-full bg-Primary-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="60"
-              height="60"
-              viewBox="0 0 60 60"
-              fill="none"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M53.8631 12.7774C54.5661 13.4807 54.9611 14.4343 54.9611 15.4287C54.9611 16.423 54.5661 17.3767 53.8631 18.0799L25.7556 46.1874C25.3842 46.559 24.9432 46.8537 24.4578 47.0547C23.9724 47.2558 23.4522 47.3593 22.9269 47.3593C22.4015 47.3593 21.8813 47.2558 21.3959 47.0547C20.9106 46.8537 20.4696 46.559 20.0981 46.1874L6.13311 32.2249C5.77495 31.879 5.48927 31.4652 5.29273 31.0077C5.0962 30.5502 4.99275 30.0581 4.98842 29.5602C4.9841 29.0623 5.07898 28.5685 5.26753 28.1076C5.45608 27.6467 5.73453 27.228 6.08663 26.8759C6.43873 26.5238 6.85742 26.2454 7.31828 26.0568C7.77915 25.8683 8.27294 25.7734 8.77087 25.7777C9.26879 25.7821 9.76086 25.8855 10.2184 26.082C10.6759 26.2786 11.0897 26.5643 11.4356 26.9224L22.9256 38.4124L48.5581 12.7774C48.9064 12.4289 49.3199 12.1525 49.775 11.9639C50.2301 11.7753 50.718 11.6782 51.2106 11.6782C51.7033 11.6782 52.1911 11.7753 52.6462 11.9639C53.1014 12.1525 53.5149 12.4289 53.8631 12.7774Z"
-                fill="white"
-              />
-            </svg>
+        bg-top bg-no-repeat max-tablet:bg-cover flex items-end justify-center"
+      >
+        <div className="flex p-5 rounded-full bg-Brand-500">
+          <IconCheck className=" shrink-0" />
+        </div>
+      </div>
+      <div className="mt-6 text-center items-center f-col gap-2">
+        <h3 className="Headings-H3">Demande validée avec succès</h3>
+        <p className="text-Text-Body Text-M">
+          Merci, votre demande a bien été prise en compte.
+        </p>
+      </div>
+      <div className="f-col gap-2 w-full w-full max-w-[1180px]">
+        <div className="p-2 bg-Sage-Gray-Lowest rounded-3xl">
+          <div className="p-5 rounded-2xl bg-white f-col items-end shadow-md">
+            <div className="f-col gap-5 w-full">
+              <h5 className="Headings-H5">Votre demande</h5>
+              <ul className="grid grid-cols-2 gap-x-4 gap-y-2">
+                <span className="Text-M text-Sage-Gray-Higher">Référence</span>
+                <span className="button-s">TRT-45821</span>
+
+                <span className="Text-M text-Sage-Gray-Higher">Produit</span>
+                <span className="button-s">Assurance Schengen</span>
+
+                <span className="Text-M text-Sage-Gray-Higher">Durée</span>
+                <span className="button-s">6 mois</span>
+
+                <span className="Text-M text-Sage-Gray-Higher">
+                  Option véhicule
+                </span>
+                <span className="button-s">Oui</span>
+
+                <span className="Text-M text-Sage-Gray-Higher">
+                  Mode de paiement
+                </span>
+                <span className="button-s">Paiement en agence</span>
+              </ul>
+            </div>
+            <div className="f-col">
+              <span className="Text-M text-Sage-Gray-High">Montant</span>
+              <div className="flex gap-2 items-baseline">
+                <span className="Headings-H2">500 DH</span>
+                <span className="Text-M text-Sage-Gray-High">TTC</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="p-2 bg-Sage-Gray-Low rounded-3xl">
+          <div className="p-5 rounded-2xl bg-white flex items-start justify-between gap-3 shadow-md">
+            <ul className="f-col gap-1">
+              <li className="f-col gap-3">
+                <span className="Headings-H5">Prochaine étape :</span>
+                <span className="Text-M text-Text-Body">
+                  Présentez-vous dans l’une de nos agences avec votre référence
+                  :
+                </span>
+              </li>
+              <li className="f-col gap-3">
+                <span className="Headings-H5">TRT-45821</span>
+                <span className="Text-M text-Text-Body">
+                  Un conseiller finalisera votre contrat.
+                </span>
+              </li>
+            </ul>
+            <div className="flex items-center gap-1 rounded-full p-1 bg-Sage-Gray-Lower">
+              <IconInfo className=" shrink-0" />
+              <span className="button-s text-Sage-Gray-Higher">
+                Nos équipes sont disponibles du lundi au vendredi
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="p-2 bg-Sage-Gray-Low rounded-3xl">
+          <div className="p-5 rounded-2xl bg-white flex justify-between gap-3 shadow-md">
+            <div className="f-col gap-3">
+              <span className="Headings-H5">Prochaine étape :</span>
+              <div className="f-col">
+                <span className="Text-M text-Text-Body">
+                  Les coordonnées bancaires vous ont été envoyées par email.
+                </span>
+                <span className="Text-M text-Text-Body">
+                  Merci d’indiquer votre référence dans le motif :
+                </span>
+                <span className="Headings-H4 mt-1">TRT-45821</span>
+              </div>
+            </div>
+            <div className="f-col items-end justify-between">
+              <div className="flex items-center gap-1 rounded-full p-1 bg-Sage-Gray-Lower">
+                <IconInfo className=" shrink-0" />
+                <span className="button-s text-Sage-Gray-Higher">
+                  Vérifiez votre boîte de réception (et les spams)
+                </span>
+              </div>
+              <button
+                type="button"
+                className="flex items-center gap-1 py-2 px-4 rounded-full bg-Sage-Gray-Lower
+          transition hover:bg-Sage-Gray-Low cursor-pointer button-s"
+              >
+                <span>Renvoyer l’email</span>
+                <IconRefresh className=" shrink-0" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="f-col gap-6 items-center">
-        <div className="text-center items-center f-col gap-2">
-          <h3 className="text-Neutral-Dark Headings-H3">
-            Merci, votre demande est bien envoyée
-          </h3>
-          <p className="text-Text-Body Text-M max-w-[450px]">
-            Nous avons bien reçu vos informations. Un conseiller vous contactera
-            sous 24h pour finaliser votre devis.
-          </p>
-        </div>
-        <div className="flex max-mobile:flex-col gap-3">
-          <ButtonLink
-            href="/"
-            label="Retour à l’accueil"
-            size="large"
-            iconClassName="w-7 h-7"
-            color="white"
-            ghost
-            reverse
-            direction="left"
-          />
-          <ButtonLink
-            href="/contact"
-            label="Contactez-nous"
-            size="large"
-            iconClassName="w-7 h-7"
-            color="brand"
-          />
-        </div>
+      <div className="flex max-mobile:flex-col gap-3">
+        <ButtonLink
+          href="/"
+          label="Retour à l’accueil"
+          size="large"
+          iconClassName="w-7 h-7"
+          color="white"
+          ghost
+          reverse
+          direction="left"
+        />
+        <button
+          type="button"
+          className="flex items-center gap-2 py-3 px-5 rounded-full bg-Sage-Gray-Lower
+          transition hover:bg-Sage-Gray-Low cursor-pointer Button-M"
+        >
+          <IconDownload className=" shrink-0" />
+          <span>Télécharger le récapitulatif (PDF)</span>
+        </button>
       </div>
     </section>
   );
